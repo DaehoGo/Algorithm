abfbbfb3da28f724e127f6dccc2944d2445a1f11
DaehoGo <113659537+DaehoGo@users.noreply.github.com>
2025-03-19 16:05:57 +0900

    [level 2] Title: 기능개발, Time: 0.22 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < progresses.length; i++){
+            if((100 - progresses[i]) % speeds[i] == 0)
+                progresses[i] = Integer.valueOf((100 - progresses[i]) / speeds[i]);
+            else
+                progresses[i] = Integer.valueOf((100 - progresses[i]) / speeds[i] + 1);
+        }
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        stack.push(progresses[0]);
+        int count = 1;
+        int idx = 0;
+        for(int i = 1; i < progresses.length; i++){
+            if(stack.peek() >= progresses[i]){
+                count++;
+            }
+            else{
+                stack.push(progresses[i]);
+                idx = i;
+                arr.add(count);
+                count = 1;
+            }
+        }
+        arr.add(count);
+        int[] answer = new int[arr.size()];
+        for(int i = 0; i < arr.size(); i++){
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
